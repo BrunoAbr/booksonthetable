6556d834035959b389f5e72d8acde8746719be97
Bruno Abreu <109926059+BrunoAbr@users.noreply.github.com>
2026-07-24 15:19:01 -0300

    feat: connecting APIs (get, post) to the database

--- backend/app/api/books.py
+++ backend/app/api/books.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.book import Book, BookCreate
+from app.schemas.book import BookBase, BookCreate, BookResponse
 from app.services.book_service import get_books, create_book
 from app.core.dependencies import get_db
 
 
 router = APIRouter()
 
-@router.get("/books")
+@router.get("/books", response_model=list[BookResponse])
 def list_books(db: Session = Depends(get_db)):
-    return get_books()
 
-@router.post("/books", response_model=Book)
-def add_book(book: BookCreate):
-    return create_book(book)
+    return get_books(db)
+
+@router.post("/books", response_model=BookResponse)
+def add_book(book: BookCreate, db: Session = Depends(get_db)):
+    return create_book(db, book)
