bd104123f1e12447e374367322fcfd7642b6b0c2
Bruno Abreu <109926059+BrunoAbr@users.noreply.github.com>
2026-07-24 15:19:55 -0300

    feat: connecting APIs (get, post) to the database

--- backend/app/services/book_service.py
+++ backend/app/services/book_service.py
@@ -1,19 +1,35 @@
-from app.schemas.book import Book, BookCreate
-from app.repositories.book_repository import find_all, save
+from app.schemas.book import BookBase, BookCreate, BookResponse
+from app.repositories import book_repository
 from sqlalchemy.orm import Session
 
 
 
 
-def get_books() -> list[Book]:
-    return find_all()
+def get_books(db: Session) -> list[BookResponse]:
+    
+    all_books = book_repository.find_all(db)
 
-def create_book(book: BookCreate) -> Book:
-    books = find_all()
+    books_response = []
 
-    new_book = Book(
-        id=len(books) +1,
-        **book.model_dump()
+    for book in all_books:
+        books_response.append(
+            BookResponse(
+                id=book.id,
+                title=book.title,
+                author=book.author,
+                pages=book.pages,
+                rating=book.rating
+            )
+        )
+    return books_response
+
+def create_book(db: Session, book: BookCreate) -> BookResponse:
+    books_model = book_repository.create(db, book)
+
+    return BookResponse(
+        id = books_model.id,
+        title = books_model.title,
+        author = books_model.author,
+        pages = books_model.pages,
+        rating = books_model.rating
     )
-
-    return save(new_book)
